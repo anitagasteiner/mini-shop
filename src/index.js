f1062a825de7f782829591a7bbe5ab09cfd42fc6
anitagasteiner <anita.gasteiner@posteo.de>
2026-02-02 13:50:18 +0100

    used bootstrap for navbar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Navbar from "./components/navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<Navbar/>);
-
-
