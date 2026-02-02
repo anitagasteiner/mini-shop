2aaee5c6f0d13080cd15f1d48ef0d08b12e234df
anitagasteiner <anita.gasteiner@posteo.de>
2026-02-02 17:22:00 +0100

    img src props optimized

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -4,7 +4,7 @@ class Product extends Component {
     state = {  } 
     render() { 
         return <div className="card" style={{width: '18rem'}}>
-                    <img src={this.props.img} className="card-img-top" alt={this.props.alt}/>
+                    <img src={"/assets/img/" + this.props.img + ".jpg"} className="card-img-top" alt={this.props.alt}/>
                     <div className="card-body">
                         <h5 className="card-title">{this.props.title}</h5>
                         <p className="card-text">{this.props.description}</p>
