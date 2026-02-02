38bc2000f51bfad6d883cda06ab18ccfa6c19a3c
anitagasteiner <anita.gasteiner@posteo.de>
2026-02-02 17:08:57 +0100

    product image title and description props added

--- src/app.jsx
+++ src/app.jsx
@@ -9,9 +9,12 @@ class App extends Component {
         return <React.Fragment>
                 <Navbar/>
                 <div className="product-container">
-                    <Product/>
-                    <Product/>
-                    <Product/>
+                    <Product title="Converse" img="/assets/img/converse_red.jpg" alt="Red Converse" description="Classic red Converse shoes."/>
+                    <Product title="White and Blue Running Shoes" img="/assets/img/running_shoes_blue.jpg" alt="White and Blue Running Shoes" description="High quality running shoes with classic design."/>
+                    <Product title="Colored Running Shoes" img="/assets/img/running_shoes_colored.jpg" alt="Colored Runnung Shoes" description="High quality running shoes with modern design."/>
+                    <Product title="Bright Running Shoes" img="/assets/img/running_shoes_bright.jpg" alt="Bright Running Shoes" description="High quality running shoes with elegant design."/>
+                    <Product title="Converse Heroes" img="/assets/img/shoes_heroes.jpg" alt="Converse with Comic Heroes" description="Converse shoes with comic heroes design."/>
+                    <Product title="Sneakers" img="/assets/img/sneakers.jpg" alt="Blue and Beige Sneakers" description="Elegant sneakers in dark blue and beige."/>
                 </div>                
             </React.Fragment>;
     }
--- src/components/product.jsx
+++ src/components/product.jsx
@@ -4,10 +4,10 @@ class Product extends Component {
     state = {  } 
     render() { 
         return <div className="card" style={{width: '18rem'}}>
-                    <img src="/assets/img/converse_red.jpg" className="card-img-top" alt="Converse"/>
+                    <img src={this.props.img} className="card-img-top" alt={this.props.alt}/>
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                        <h5 className="card-title">{this.props.title}</h5>
+                        <p className="card-text">{this.props.description}</p>
                         <a href="/" className="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>;
