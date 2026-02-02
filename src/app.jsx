c5f41d7c2d8393ee52c5eb44e005db37cae0fc4b
anitagasteiner <anita.gasteiner@posteo.de>
2026-02-02 16:38:50 +0100

    root component added

---
+++ src/app.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Navbar from "./components/navbar";
+import Product from "./components/product";
+import "./app.css";
+
+class App extends Component {
+    state = {  } 
+    render() { 
+        return <React.Fragment>
+                <Navbar/>
+                <div className="product-container">
+                    <Product/>
+                    <Product/>
+                    <Product/>
+                </div>                
+            </React.Fragment>;
+    }
+}
+ 
+export default App;
+
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Navbar from "./components/navbar";
-import Product from "./components/product";
 import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./app";
+
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<Product/>);
+root.render(<App/>);
+
