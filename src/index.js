a5fb717110af145d823b51c046708b3d885a9d56
anitagasteiner <anita.gasteiner@posteo.de>
2026-02-02 11:39:14 +0100

    createRoot().render() instead of ReactDom.render()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import React from "react"
-import ReactDom from "react-dom"
+import React from "react";
+import { createRoot } from "react-dom/client";
+// import ReactDom from "react-dom"
+
+const element = <h1>Hello World</h1>;
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(element);
+
+// ReactDom.render(element, document.getElementById('root'));
 
