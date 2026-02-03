f667eabe2536887582fc6536a2537247250e4d59
anitagasteiner <anita.gasteiner@posteo.de>
2026-02-03 10:31:24 +0100

    new component for shopping cart added

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from "./components/navbar";
 import Product from "./components/product";
+import ShoppingCart from './components/shopping-cart';
 import "./app.css";
 
 class App extends Component {
@@ -15,7 +16,8 @@ class App extends Component {
                     <Product title="Bright Running Shoes" img="running-shoes-bright" alt="Bright Running Shoes" description="High quality running shoes with elegant design."/>
                     <Product title="Converse Heroes" img="shoes-heroes" alt="Converse with Comic Heroes" description="Converse shoes with comic heroes design."/>
                     <Product title="Sneakers" img="sneakers" alt="Blue and Beige Sneakers" description="Elegant sneakers in dark blue and beige."/>
-                </div>                
+                </div>
+                <ShoppingCart/>
             </React.Fragment>;
     }
 }
