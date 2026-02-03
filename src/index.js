3984a11af709fc0b3c05b641251b241fd7294aa5
anitagasteiner <anita.gasteiner@posteo.de>
2026-02-03 11:15:14 +0100

    index.js render changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOMClient from "react-dom/client";
+// import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./app";
 
 
 const container = document.getElementById('root');
-const root = createRoot(container);
+// const root = createRoot(container);
+const root = ReactDOMClient.createRoot(container);
 root.render(<App/>);
 
