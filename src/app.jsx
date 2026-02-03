f07167da1b8269749db42face7bd5e846c861ae9
anitagasteiner <anita.gasteiner@posteo.de>
2026-02-03 10:28:53 +0100

    img names changed

--- src/app.jsx
+++ src/app.jsx
@@ -9,11 +9,11 @@ class App extends Component {
         return <React.Fragment>
                 <Navbar/>
                 <div className="product-container">
-                    <Product title="Converse" img="converse_red" alt="Red Converse" description="Classic red Converse shoes."/>
-                    <Product title="White and Blue Running Shoes" img="running_shoes_blue" alt="White and Blue Running Shoes" description="High quality running shoes with classic design."/>
-                    <Product title="Colored Running Shoes" img="running_shoes_colored" alt="Colored Runnung Shoes" description="High quality running shoes with modern design."/>
-                    <Product title="Bright Running Shoes" img="running_shoes_bright" alt="Bright Running Shoes" description="High quality running shoes with elegant design."/>
-                    <Product title="Converse Heroes" img="shoes_heroes" alt="Converse with Comic Heroes" description="Converse shoes with comic heroes design."/>
+                    <Product title="Converse" img="converse-red" alt="Red Converse" description="Classic red Converse shoes."/>
+                    <Product title="White and Blue Running Shoes" img="running-shoes-blue" alt="White and Blue Running Shoes" description="High quality running shoes with classic design."/>
+                    <Product title="Colored Running Shoes" img="running-shoes-colored" alt="Colored Runnung Shoes" description="High quality running shoes with modern design."/>
+                    <Product title="Bright Running Shoes" img="running-shoes-bright" alt="Bright Running Shoes" description="High quality running shoes with elegant design."/>
+                    <Product title="Converse Heroes" img="shoes-heroes" alt="Converse with Comic Heroes" description="Converse shoes with comic heroes design."/>
                     <Product title="Sneakers" img="sneakers" alt="Blue and Beige Sneakers" description="Elegant sneakers in dark blue and beige."/>
                 </div>                
             </React.Fragment>;
