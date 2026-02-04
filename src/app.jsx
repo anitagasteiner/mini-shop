228f4cf63416181f091339f04705f2ffc76e9f38
anitagasteiner <anita.gasteiner@posteo.de>
2026-02-04 10:17:47 +0100

    add items to shopping cart: function prepared

--- src/app.jsx
+++ src/app.jsx
@@ -5,18 +5,32 @@ import ShoppingCart from './components/shopping-cart';
 import "./app.css";
 
 class App extends Component {
-    state = {  } 
+    state = { 
+        items: []
+     }
+
+    addItem = (amount, name, price) => {
+        let currentItems = this.state.items;
+        currentItems.push({
+            amount,
+            name,
+            price,
+        });
+        this.setState({items: currentItems});
+        console.log(this.state);
+    }
+
     render() { 
         return <React.Fragment>
                 <Navbar/>
                 <div className="main-container">
                     <div className="product-container">
-                        <Product title="Red Converse" img="converse-red" alt="Red Converse" description="Classic red Converse shoes."/>
-                        <Product title="White and Blue Running Shoes" img="running-shoes-blue" alt="White and Blue Running Shoes" description="High quality running shoes with classic design."/>
-                        <Product title="Colored Running Shoes" img="running-shoes-colored" alt="Colored Runnung Shoes" description="High quality running shoes with modern design."/>
-                        <Product title="Bright Running Shoes" img="running-shoes-bright" alt="Bright Running Shoes" description="High quality running shoes with elegant design."/>
-                        <Product title="Converse Heroes" img="shoes-heroes" alt="Converse with Comic Heroes" description="Converse shoes with comic heroes design."/>
-                        <Product title="Sneakers" img="sneakers" alt="Blue and Beige Sneakers" description="Elegant sneakers in dark blue and beige."/>
+                        <Product onAdd={() => this.addItem(1, "Red Converse", 59.99)} title="Red Converse" img="converse-red" alt="Red Converse" description="Classic red Converse shoes."/>
+                        <Product onAdd={() => this.addItem(1, "White and Blue Running Shoes", 86.95)} title="White and Blue Running Shoes" img="running-shoes-blue" alt="White and Blue Running Shoes" description="High quality running shoes with classic design."/>
+                        <Product onAdd={() => this.addItem(1, "Colored Running Shoes", 119.95)} title="Colored Running Shoes" img="running-shoes-colored" alt="Colored Runnung Shoes" description="High quality running shoes with modern design."/>
+                        <Product onAdd={() => this.addItem(1, "Bright Running Shoes", 94.95)} title="Bright Running Shoes" img="running-shoes-bright" alt="Bright Running Shoes" description="High quality running shoes with elegant design."/>
+                        <Product onAdd={() => this.addItem(1, "Converse Heroes", 64.99)} title="Converse Heroes" img="shoes-heroes" alt="Converse with Comic Heroes" description="Converse shoes with comic heroes design."/>
+                        <Product onAdd={() => this.addItem(1, "Sneakers", 75.99)} title="Sneakers" img="sneakers" alt="Blue and Beige Sneakers" description="Elegant sneakers in dark blue and beige."/>
                     </div>
                     <ShoppingCart/>
                 </div>
@@ -25,5 +39,4 @@ class App extends Component {
 }
  
 export default App;
-
 
--- src/components/product.jsx
+++ src/components/product.jsx
@@ -8,7 +8,7 @@ class Product extends Component {
                     <div className="card-body">
                         <h5 className="card-title">{this.props.title}</h5>
                         <p className="card-text">{this.props.description}</p>
-                        <a href="/" className="btn btn-primary">Go somewhere</a>
+                        <button onClick={this.props.onAdd} className="btn btn-primary">Add to Shopping Cart</button>
                     </div>
                 </div>;
     }
