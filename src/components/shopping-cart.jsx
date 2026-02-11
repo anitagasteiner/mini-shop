38c1484a9d632b95df515410e737edb1436640da
anitagasteiner <anita.gasteiner@posteo.de>
2026-02-11 16:07:14 +0100

    correction class to className

--- src/components/shopping-cart.jsx
+++ src/components/shopping-cart.jsx
@@ -6,12 +6,12 @@ class ShoppingCart extends Component {
         return <div className="shopping-cart-container">
             <h2>Shopping Cart</h2>
 
-            <ul class="list-group">
-                { this.props.items.map(item => <li class="list-group-item d-flex justify-content-left align-items-center shopping-cart-list-item" key={ item.name }>
-                    <span class="badge text-bg-primary rounded-pill">{ item.amount }</span>
+            <ul className="list-group">
+                { this.props.items.map(item => <li className="list-group-item d-flex justify-content-left align-items-center shopping-cart-list-item" key={ item.name }>
+                    <span className="badge text-bg-primary rounded-pill">{ item.amount }</span>
                     { item.name } <br/> { item.price } €
                     </li>) }
-                <li class="list-group-item active text-end"><b>Total: { this.props.sum } €</b></li>
+                <li className="list-group-item active text-end"><b>Total: { this.props.sum } €</b></li>
             </ul>
 
         </div>;
