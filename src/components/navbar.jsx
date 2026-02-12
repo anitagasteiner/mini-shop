695cb41b981ef5cd9ad792977051a0186fb18bcc
anitagasteiner <anita.gasteiner@posteo.de>
2026-02-12 09:33:43 +0100

    bootstrap navbar expanded

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,9 +3,20 @@ import React, { Component } from 'react';
 class Navbar extends Component {
     state = {  } 
     render() { 
-        return <nav className="navbar bg-body-tertiary">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="#">Navbar</a>
+        return <nav className="navbar navbar-expand-lg navbar-light">
+                    <a className="navbar-brand" href="/">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                        <a className="nav-item nav-link active" href="/">Home
+                            <span className="sr-only">(current)</span>
+                        </a>
+                        <a className="nav-item nav-link" href="/">Features</a>
+                        <a className="nav-item nav-link" href="/">Pricing</a>
+                        <a className="nav-item nav-link disabled" href="/">Disabled</a>
+                        </div>
                     </div>
                 </nav>;
     }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOMClient from "react-dom/client";
 // import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 import App from "./app";
 
